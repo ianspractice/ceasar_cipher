cdcd43956e4ea9a3c5ed8e4d005ebb472faf879c
ianspractice <bodybody.ian@gmail.com>
2025-05-21 13:13:30 -0600

    Combine letter_shift methods into one

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -9,35 +9,27 @@ def new_letter(index)
 end
 
 def letter_shift(letter, shift)
-  initial_index = letter_index(letter)
+  down_letter = letter.downcase
+  initial_index = letter_index(down_letter)
   new_index = initial_index - 26 + (shift % 26)
   new_letter = new_letter(new_index)
   new_letter
 end
 
-def letter_shift_up(letter, shift)
-  down_letter = letter.downcase
-  initial_index = letter_index(down_letter)
-  new_index = initial_index - 26 + (shift % 26)
-  new_letter = new_letter(new_index)
-  new_letter.upcase
-end
-
 def shift(string, shift)
-  array = string.split("")
   new_array = []
-  array.map do |letter|
+  string.split("").map do |letter|
     if letter.match(/[^a-zA-z]/)
       new_letter = letter
     elsif letter == letter.upcase
-      new_letter = letter_shift_up(letter, shift)
+      new_letter = letter_shift(letter, shift).upcase
     else
       new_letter = letter_shift(letter, shift)
     end
   new_array.push(new_letter)
   end
   new_string = new_array.join("")
-  p new_string
+  new_string
 end
 
-shift("Aapple?? 888 *** ButtTTR!!", 27)
+puts shift("abcdef ABCDEF 1234 !*?{}", 27)
